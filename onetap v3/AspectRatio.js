86c979069e0d29be4e5a140838b8510bb351a52f
sonka <83928927+prxtenses@users.noreply.github.com>
2021-08-15 13:20:36 -0300

    Updated aspectratio
    Organized a bit

--- onetap v3/AspectRatio.js
+++ onetap v3/AspectRatio.js
@@ -11,10 +11,9 @@ UI.AddSliderFloat( "Aspect Ratio", 0.0, 5.0 );
 UI.AddLabel("<---------------------------------------->");
 
 function aspectratio( ) {
-menu_val = UI.GetValue("Aspect Ratio");
-string_menu_val = menu_val.toString();
-
-Convar.SetString ("r_aspectratio", string_menu_val );
+  menu_val = UI.GetValue("Aspect Ratio");
+  string_menu_val = menu_val.toString();
+  Convar.SetString ("r_aspectratio", string_menu_val );
 }
 
 Cheat.RegisterCallback( "FrameStageNotify", "aspectratio" );
